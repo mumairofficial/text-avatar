03ca62e5731253543e02d7f770821fb94c1399e5
mumair <mumairofficial@gmail.com>
2017-05-12 02:44:55 +0500

    code refactoring

--- dist/text-avatar.ts
+++ dist/text-avatar.ts
@@ -1,32 +1,29 @@
-import {Directive, ElementRef, Input, SimpleChanges} from '@angular/core';
-import {ColorGenerator} from "./color-generator";
+import { Directive, ElementRef, Input, SimpleChanges } from '@angular/core';
+import { ColorGenerator } from "./color-generator";
 
-/**
- * Generated class for the TextAvatar directive.
- *
- * See https://angular.io/docs/ts/latest/api/core/index/DirectiveMetadata-class.html
- * for more info on Angular Directives.
- */
 @Directive({
-	selector: 'text-avatar',
-	providers: [ColorGenerator]
+  selector: 'text-avatar',
+  providers: [ColorGenerator]
 })
-export class TextAvatar {
+export class TextAvatarDirective {
 
-	constructor(private element: ElementRef, private colorGenerator: ColorGenerator){  }
+  constructor(private element: ElementRef, private colorGenerator: ColorGenerator) { }
 
-	@Input() text: string;
-	@Input() color: string;
-	ngOnChanges(changes: SimpleChanges){
-		let text = changes['text'] ? changes['text'].currentValue : '';
-		let color = changes['color'] ? changes['color'].currentValue : '';
-		if(text){
-			this.element.nativeElement.setAttribute("value", text.charAt(0));
-			if(color){
-				this.element.nativeElement.style.backgroundColor = color;
-			}else{
-				this.element.nativeElement.style.backgroundColor = this.colorGenerator.getColor(text);
-			}
-		}
-	}
+  @Input() text: string;
+  @Input() color: string;
+  ngOnChanges(changes: SimpleChanges) {
+    let text = changes['text'] ? changes['text'].currentValue : null;
+    let color = changes['color'] ? changes['color'].currentValue : null;
+
+    this.element.nativeElement.setAttribute("value", this.extractFirstCharacter(text));
+    this.element.nativeElement.style.backgroundColor = this.backgroundColorHexString(color, text);
+  }
+
+  private extractFirstCharacter(text: string): string {
+    return text.charAt(0) || '';
+  }
+
+  private backgroundColorHexString(color: string, text: string): string {
+    return color || this.colorGenerator.getColor(this.extractFirstCharacter(text));
+  }
 }
