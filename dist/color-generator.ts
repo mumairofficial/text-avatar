307688244fc036f52eeacbee5c061296994c89e3
mumair <mumairofficial@gmail.com>
2017-05-14 22:52:17 +0500

    code refactoring

--- dist/color-generator.ts
+++ dist/color-generator.ts
@@ -3,18 +3,18 @@ export class ColorGenerator {
     '#4fc3f7', '#4dd0e1', '#4db6ac', '#81c784', '#aed581', '#ff8a65', '#d4e157', '#673ab7',
     '#ffb74d', '#a1887f', '#90a4ae'];
 
-  getColor(str: string): string {
-    return this.COLORS[Math.abs(this.generateHashCode(str)) % this.COLORS.length];
+  public getColor(str: string): string {
+    return this.COLORS[Math.abs(this.toNumber(str)) % this.COLORS.length];
   }
 
-  generateHashCode(str: string): number {
+  private toNumber(str: string): number {
     let h = 0;
-    if (str.length > 0) {
-      for (let i = 0; i < str.length; i++) {
-        h = 31 * h + str.charCodeAt(i);
-        h |= 0; // Convert to 32bit integer
-      }
+
+    for (let i = 0; i < str.length; i++) {
+      h = 31 * h + str.charCodeAt(i);
+      h |= 0; // Convert to 32bit integer
     }
+    
     return h;
   };
 }
